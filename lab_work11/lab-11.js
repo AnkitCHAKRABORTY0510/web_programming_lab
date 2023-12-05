0e44eeacada17d66f35d75f2fe40d1265a98e6a5
ANKIT CHAKRABORTY <77633006+AnkitCHAKRABORTY0510@users.noreply.github.com>
2023-12-06 00:59:04 +0530

    Add files via upload

---
+++ lab_work11/lab-11.js
@@ -0,0 +1,27 @@
+function check()
+{
+        var name = document.calc.name1.value;
+        var debit1=document.calc.debit1.value;
+        if(name.length<1)
+        {
+                alert("Empty");
+        }
+        var card = /^(?:[1-9]{1}[0-9]{15})$/;
+        if (!(debit1.match(card)))
+        {
+                alert("Enter 16 digits debit card number.");
+        }
+      
+        var month1 = document.calc.m1.value;
+        if(month1<1 || month1>12)
+        {
+            alert("check Month value");
+        }
+        var year1 = document.calc.yr1.value;
+        {
+            if(year1.length!=2)
+            {
+                alert("check year value");
+            }
+        }
+}
